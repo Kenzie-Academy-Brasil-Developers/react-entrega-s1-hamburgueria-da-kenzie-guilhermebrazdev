34b75c4c4b7158eaec3510865971576a42129b05
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2021-11-11 16:58:33 -0300

    corrigindo estilização e responsividade para telas

--- src/components/Carrinho/index.jsx
+++ src/components/Carrinho/index.jsx
@@ -2,48 +2,52 @@ import "./carrinho.css";
 
 function Carrinho({ onShop, removeAll, removeItem }) {
   return (
-    <div id="carrinho">
-      <div id="titleCart">Carrinho de compras</div>
-      <ul>
-        {onShop.map((element, index) => (
-          <li key={index} id="itemList">
-            <img src={element.img} />
-
-            <div id="descricao">
-              <h3>{element.name}</h3>
-              <p>{element.category}</p>
-            </div>
-            <button id="removerBotao" onClick={() => removeItem(element.id)}>
-              Remover
-            </button>
-          </li>
-        ))}
-      </ul>
-      {onShop.length === 0 ? (
-        <div>
-          <h1>Sua sacola está vazia</h1>
-          <p>Adicione itens</p>
+    <div id="cartBox">
+      <div id="carrinho">
+        <div id="titleCart">
+          <h4>Carrinho de compras</h4>
         </div>
-      ) : (
-        <div>
-          <div id="price">
-            <p id="total">Total</p>
-            <p>
-              R$
-              {onShop
-                .reduce(
-                  (acc, currentValue) => acc + Number(currentValue.price),
-                  0
-                )
-                .toFixed(2)}
-            </p>
+        <ul>
+          {onShop.map((element, index) => (
+            <li key={index} id="itemList">
+              <img src={element.img} />
+
+              <div id="descricao">
+                <h3>{element.name}</h3>
+                <p>{element.category}</p>
+              </div>
+              <button id="removerBotao" onClick={() => removeItem(element.id)}>
+                Remover
+              </button>
+            </li>
+          ))}
+        </ul>
+        {onShop.length === 0 ? (
+          <div>
+            <h1>Sua sacola está vazia</h1>
+            <p>Adicione itens</p>
           </div>
+        ) : (
+          <div>
+            <div id="price">
+              <p id="total">Total</p>
+              <p>
+                R$
+                {onShop
+                  .reduce(
+                    (acc, currentValue) => acc + Number(currentValue.price),
+                    0
+                  )
+                  .toFixed(2)}
+              </p>
+            </div>
 
-          <button id="removeAllBotao" onClick={() => removeAll([])}>
-            Remover todos
-          </button>
-        </div>
-      )}
+            <button id="removeAllBotao" onClick={() => removeAll([])}>
+              Remover todos
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
